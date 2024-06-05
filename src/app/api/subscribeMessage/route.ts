94bc604510b95fd21972e761bf7d2d06a8d6a3a6
Fariz Apriyanto <fapriyanto@alterra.id>
2024-06-05 16:56:58 +0700

    fix: sse connection

--- src/app/api/subscribeMessage/route.ts
+++ src/app/api/subscribeMessage/route.ts
@@ -1,52 +1,59 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import amqp, { Message } from "amqplib";
+import { NextApiRequest, NextApiResponse } from "next"
+import amqp, { Message } from "amqplib"
+import { NextResponse } from "next/server"
 
-const RABBITMQ_URL =
-  "amqps://lvqhizkj:wlODxXijHupv4JbrB90ivkLrLlJjRorI@octopus.rmq3.cloudamqp.com/lvqhizkj";
+const RABBITMQ_URL = "amqps://lvqhizkj:wlODxXijHupv4JbrB90ivkLrLlJjRorI@octopus.rmq3.cloudamqp.com/lvqhizkj"
 
-let connection: amqp.Connection | any = null;
-let channel: amqp.Channel | any = null;
+let connection: amqp.Connection | any = null
+let channel: amqp.Channel | any = null
 
 async function setupRabbitMQ() {
-  connection = await amqp.connect(RABBITMQ_URL);
-  channel = await connection.createChannel();
-  await channel.assertQueue("audio_queue", { durable: false });
+  connection = await amqp.connect(RABBITMQ_URL)
+  channel = await connection.createChannel()
+  await channel.assertQueue("audio_queue", { durable: false })
 }
 
+export const runtime = "nodejs"
+// This is required to enable streaming
+export const dynamic = "force-dynamic"
+
 export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "GET") {
-    res.setHeader("Content-Type", "text/event-stream");
-    res.setHeader("Cache-Control", "no-cache");
-    res.setHeader("Connection", "keep-alive");
-    res.status(200).end();
-
-    try {
-      if (!connection || !channel) {
-        await setupRabbitMQ();
-      }
-
-      channel.consume(
-        "audio_queue",
-        (msg: Message | null) => {
-          if (msg !== null) {
-            console.log("Received message:", msg.content.toString());
-            res.write(`data: ${msg.content.toString()}\n\n`);
-            channel.ack(msg);
-          }
-        },
-        { noAck: false }
-      );
-
-      req.on("close", () => {
-        console.log("Client disconnected");
-      });
-    } catch (error) {
-      console.error("Error subscribing to messages:", error);
-      res
-        .status(500)
-        .json({ success: false, message: "Error subscribing to messages" });
+  const encoder = new TextEncoder()
+  try {
+    if (!connection || !channel) {
+      await setupRabbitMQ()
     }
-  } else {
-    res.status(405).json({ success: false, message: "Method not allowed" });
+
+    const stream = new ReadableStream({
+      start(controller) {
+        // Subscribe to Redis updates for the key: "posts"
+        // In case of any error, just log it
+        channel.consume(
+          "audio_queue",
+          (msg: Message | null) => {
+            if (msg !== null) {
+              console.log("Received message:", msg.content.toString())
+              controller.enqueue(encoder.encode("data: " + msg.content.toString() + "\n\n"))
+              channel.ack(msg)
+            }
+          },
+          { noAck: false },
+        )
+      },
+    })
+
+    console.log("SSE Connection opened")
+    return new NextResponse(stream, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "text/event-stream; charset=utf-8",
+        Connection: "keep-alive",
+        "Cache-Control": "no-cache, no-transform",
+        "Content-Encoding": "none",
+      },
+    })
+  } catch (error) {
+    console.error("Error subscribing to messages:", error)
+    res.status(500).json({ success: false, message: "Error subscribing to messages" })
   }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,52 @@
-"use client";
+"use client"
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react"
 const PlayVideo: React.FC = () => {
-  const video1 =
-    "https://res.cloudinary.com/dp8ita8x5/video/upload/v1717566444/videoStream/comhm3zcijnjtk0f02wm.mp4";
-  const video2 =
-    "https://res.cloudinary.com/dp8ita8x5/video/upload/v1717566116/videoStream/fzqcsqer81sdvl8hiana.mp4";
-  const [messages, setMessages] = useState<string[]>([]);
-  const [currentVideo, setCurrentVideo] = useState(video1);
-  const [loadingText, setLoadingText] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [status, setStatus] = useState("");
-  const [audioUrl, setAudioUrl] = useState("");
-  const [inputText, setInputText] = useState("");
-  const audioRef = useRef<HTMLAudioElement>(null);
+  const video1 = "https://res.cloudinary.com/dp8ita8x5/video/upload/v1717566444/videoStream/comhm3zcijnjtk0f02wm.mp4"
+  const video2 = "https://res.cloudinary.com/dp8ita8x5/video/upload/v1717566116/videoStream/fzqcsqer81sdvl8hiana.mp4"
+  const [messages, setMessages] = useState<string[]>([])
+  const [currentVideo, setCurrentVideo] = useState(video1)
+  const [loadingText, setLoadingText] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [status, setStatus] = useState("")
+  const [audioUrl, setAudioUrl] = useState("")
+  const [inputText, setInputText] = useState("")
+  const audioRef = useRef<HTMLAudioElement>(null)
 
+  const connectToStream = () => {
+    // Connect to /api/stream as the SSE API source
+    const eventSource = new EventSource("/api/subscribeMessage")
+    eventSource.addEventListener("message", (event) => {
+      console.log("Received message:", event.data)
+      setMessages((prevMessages) => [...prevMessages, event.data])
+    })
+    // In case of any error, close the event source
+    // So that it attempts to connect again
+    eventSource.addEventListener("error", () => {
+      eventSource.close()
+      setTimeout(connectToStream, 1)
+    })
+    // As soon as SSE API source is closed, attempt to reconnect
+    // @ts-ignore
+    eventSource.onclose = () => {
+      setTimeout(connectToStream, 1)
+    }
+    return eventSource
+  }
   useEffect(() => {
-    const eventSource = new EventSource("/api/subscribeMessage");
-
-    eventSource.onmessage = (event) => {
-      setMessages((prevMessages) => [...prevMessages, event.data]);
-    };
-
+    const eventSource = connectToStream()
     return () => {
-      eventSource.close();
-    };
-  }, []);
+      console.log("CLOSED")
+      eventSource.close()
+    }
+  }, [])
 
   const handleAudioEnded = () => {
     if (audioRef.current) {
-      audioRef.current.pause();
-      setAudioUrl("");
+      audioRef.current.pause()
+      setAudioUrl("")
     }
-  };
+  }
 
   return (
     <div className="grid grid-cols-3 h-[100dvh]">
@@ -52,9 +66,7 @@ const PlayVideo: React.FC = () => {
                   autoPlay
                   loop
                   muted
-                  className={`transition-opacity duration-1000 ${
-                    isLoading ? "opacity-0" : "opacity-100"
-                  }`}
+                  className={`transition-opacity duration-1000 ${isLoading ? "opacity-0" : "opacity-100"}`}
                   onEnded={handleAudioEnded}
                 >
                   <source src={currentVideo} type="video/mp4" />
@@ -83,7 +95,7 @@ const PlayVideo: React.FC = () => {
         </audio>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default PlayVideo;
+export default PlayVideo
